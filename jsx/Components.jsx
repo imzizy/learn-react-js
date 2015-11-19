65de50cceb8352fe451394b6422484796b57b44d
zizy <mezizy@live.com>
2015-11-19 14:53:36 -0600

    Fetching from the server

--- jsx/Components.jsx
+++ jsx/Components.jsx
@@ -4,16 +4,39 @@ var data = [
 ];
 
 var CommentBox = React.createClass({
+    loadCommentsFromServer: function () {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            cache: false,
+            success: function (data) {
+                this.setState({data: data});
+
+            }.bind(this),
+            error: function (xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+
+    },
+
+    getInitialState: function () {
+        return {data: []};
+    },
+    componentDidMount(){
+        this.loadCommentsFromServer();
+        setInterval(this.loadCommentsFromServer(), this.props.pollInterval);
+    },
     render: function () {
         return (
             <div className="commentBox">
                 <h1>Comments</h1>
-                <CommentList data={this.props.data}/>
+                <CommentList data={this.state.data}/>
                 <CommentForm/>
             </div>
         );
     }
-})
+});
 
 var CommentForm = React.createClass({
     render: function () {
@@ -27,12 +50,12 @@ var CommentForm = React.createClass({
 
 var CommentList = React.createClass({
     render: function () {
-        var commentNodes=this.props.data.map(function(comment){
+        var commentNodes = this.props.data.map(function (comment) {
             return (
                 <Comment author={comment.author} key={comment.id}>
                     {comment.text}
                 </Comment>
-             )
+            )
         });
         return (
             <div className="commentList">
@@ -60,9 +83,9 @@ var Comment = React.createClass({
             </div>
         );
     }
-})
+});
 
 ReactDOM.render(
-    <CommentBox data={data} />,
+    <CommentBox url='/learn-react-js/api/comments.json' pollInterval={2000} />,
     document.getElementById('container')
 );
